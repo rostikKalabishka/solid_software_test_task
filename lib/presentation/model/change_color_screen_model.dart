7aad7db8fb09b4e90fb396e070f4f28e270708e6
RostikKalabishka <rostik31052002@gmail.com>
2025-05-28 17:47:12 +0300

    add background color info

--- lib/presentation/model/change_color_screen_model.dart
+++ lib/presentation/model/change_color_screen_model.dart
@@ -5,13 +5,36 @@ class ChangeColorScreenModel extends ChangeNotifier {
   final RandomColorUseCase _randomColorUseCase = RandomColorUseCase();
   Color _backgroundColor = Colors.white;
   Color _textColor = Colors.black;
+  Color _textRandomColor = Colors.black;
+
+  String? backgroundColorInfo;
 
   Color get backgroundColor => _backgroundColor;
   Color get textColor => _textColor;
+  Color get textRandomColor => _textRandomColor;
 
   void changeColor() {
     _backgroundColor = _randomColorUseCase.generateColor();
-    _textColor = _randomColorUseCase.generateColor();
+    _textRandomColor = _randomColorUseCase.generateColor();
+    _textColor = _computeContrastTextColor(_backgroundColor);
+
+    notifyListeners();
+  }
+
+  //luminance formula 0.299*R + 0.587 * G + 0.114 * B
+
+  Color _computeContrastTextColor(Color backgroundColor) {
+    return 0.299 * backgroundColor.r +
+                0.587 * backgroundColor.g +
+                0.114 * backgroundColor.b >
+            0.4
+        ? Colors.black
+        : Colors.white;
+  }
+
+  void showInfoAboutBackgroundColor() {
+    backgroundColorInfo =
+        '''RGB:  \nRed: ${_backgroundColor.r}, \nGreen: ${_backgroundColor.g}, \nBlue: ${_backgroundColor.b}, \nin Hex: #${_backgroundColor.value.toRadixString(16)}''';
     notifyListeners();
   }
 }
--- lib/presentation/screens/change_color_screen.dart
+++ lib/presentation/screens/change_color_screen.dart
@@ -17,11 +17,41 @@ class ChangeColorScreen extends StatelessWidget {
           duration: Duration(milliseconds: 1500),
           color: model.backgroundColor,
           child: Center(
-            child: Text(
-              'Hello there',
-              style: theme.textTheme.displayMedium
-                  ?.copyWith(color: model.textColor),
-            ),
+            child: Column(
+                spacing: 20,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Hello there',
+                    style: theme.textTheme.displayMedium
+                        ?.copyWith(color: model.textColor),
+                  ),
+                  Text(
+                    'Text random color',
+                    style: theme.textTheme.displayMedium
+                        ?.copyWith(color: model.textRandomColor),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: model.textColor,
+                    ),
+                    onPressed: () => model.showInfoAboutBackgroundColor(),
+                    child: Text(
+                      'Show background info about color',
+                      style: theme.textTheme.displayMedium
+                          ?.copyWith(color: model.backgroundColor),
+                    ),
+                  ),
+                  model.backgroundColorInfo != null
+                      ? Text(
+                          model.backgroundColorInfo ?? "",
+                          style: theme.textTheme.displayMedium
+                              ?.copyWith(color: model.textColor),
+                          textAlign: TextAlign.center,
+                        )
+                      : SizedBox.shrink(),
+                ]),
           ),
         ),
       ),
